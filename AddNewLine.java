1dfc839ff273e4c21929bfb79298b027a6b6fcfa
Saya1206 <sayali.patil0831>
2025-03-04 22:33:10 +0530

    Add new line

---
+++ AddNewLine.java
@@ -0,0 +1,8 @@
+public class AddNewLine{
+  public static void main(String[] args) {
+    // System.out.print("Welcome\n");
+    System.out.println("Welcome");
+    System.out.println("First");
+    System.out.println("Java Program");
+  }
+}
