5827528928f15ae0b7bc4c856a25fc9e013d6fe2
Saya1206 <sayali.patil0831>
2025-03-05 09:16:23 +0530

    Java Variables

---
+++ Variables.java
@@ -0,0 +1,29 @@
+
+public class Variables {
+
+    public static void main(String[] args) {
+        int age = 23;
+        System.out.println(age);
+
+        age = 24;
+        System.out.println(age);
+
+        float cgpa = 8.4f;
+        System.out.println(cgpa);
+
+        double rate;
+        rate = 7.89234;
+        System.out.println(rate);
+
+        char myChar = 'S';
+        System.out.println(myChar);
+
+        String greet = "good morning";
+        System.out.println(greet);
+
+        boolean isPlaced = true;
+        System.out.println(isPlaced);
+
+        System.out.println("My age is: " + age);
+    }
+}
