51a47bad491f128926fa72c69dff45d34e8a1488
Saya1206 <sayali.patil0831>
2025-03-05 09:22:59 +0530

    Java literals

---
+++ Literals.java
@@ -0,0 +1,33 @@
+
+public class Literals {
+
+    public static void main(String[] args) {
+
+        // literals are fixed values that are assigned to variables.
+        // Integer Literals
+        int num = 12;
+        System.out.println(num);
+
+        // Floating-Point Literals
+        float myFloat = 3.2f;
+        System.out.println(myFloat);
+
+        double pi = 3.14;
+        System.out.println(pi);
+
+        // Character Literals
+        // A single character enclosed in single quotes
+        char ch = 'A';
+        System.out.println(ch);
+
+        // String Literals
+        // A sequence of characters enclosed in double quotes
+        String name = "neha";
+        System.out.println(name);
+
+        // Boolean Literals
+        boolean isFinish = false;
+        System.out.println(isFinish);
+
+    }
+}
