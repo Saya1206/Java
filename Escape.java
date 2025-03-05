d453488307cb3d10e8c2c097a9ba26a381c55992
Saya1206 <sayali.patil0831>
2025-03-05 10:40:39 +0530

    Java escape sequences

---
+++ Escape.java
@@ -0,0 +1,15 @@
+
+public class Escape {
+
+    public static void main(String[] args) {
+        System.out.println("Good\tMorning");
+
+        System.out.println("Good \nMorning");
+
+        System.out.println("Good \'Morning\'");
+
+        System.out.println("Good \"Morning\"");
+
+        System.out.println("Good \\Morning\\");
+    }
+}
