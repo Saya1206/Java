de90e3218a9e16da08d758a0399459acdabdf61b
Saya1206 <sayali.patil0831>
2025-03-08 12:00:52 +0530

    Java Arithmetic Operator

---
+++ Arithmetic.java
@@ -0,0 +1,25 @@
+
+public class Arithmetic {
+
+    public static void main(String[] args) {
+
+        int a = 13;
+        int b = 5;
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+
+        // Modulus operator to get reminder in integer division
+        System.out.println(a % b);
+
+        double x = 12.0;
+        double y = 5.0;
+
+        System.out.println(x + y);
+        System.out.println(x - y);
+        System.out.println(x * y);
+        System.out.println(x / y);
+    }
+}
