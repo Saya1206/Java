759de2efd135cc586b03eafba2060e62ddd96623
Saya1206 <sayali.patil0831>
2025-03-08 11:51:28 +0530

    Java Assignment Operator

---
+++ Assignment.java
@@ -0,0 +1,27 @@
+
+public class Assignment {
+
+    public static void main(String[] args) {
+        int num1 = 13;
+        System.out.println(num1);
+
+        int num2 = num1;
+        System.out.println(num2);
+
+        int a = 10;
+        a += 5;
+        System.out.println(a);
+
+        a -= 3;
+        System.out.println(a);
+
+        a *= 3;
+        System.out.println(a);
+
+        a /= 12;
+        System.out.println(a);
+
+        a /= 2;
+        System.out.println(a);
+    }
+}
