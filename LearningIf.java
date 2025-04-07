4e136f7988ce20fd17bb1baf650ac450feb1e43e
Saya1206 <sayali.patil0831>
2025-04-07 22:09:02 +0530

    if-else

---
+++ LearningIf.java
@@ -0,0 +1,15 @@
+
+public class LearningIf {
+
+    public static void main(String[] args) {
+
+        boolean isMale = false;
+        String name = "Raghi";
+
+        if (isMale) {
+            System.out.println("Mr. " + name);
+        } else {
+            System.out.println("Ms. " + name);
+        }
+    }
+}
