fbf8c339752080ba2dc0b07a57ed71696326a81c
Saya1206 <sayali.patil0831>
2025-04-17 12:36:17 +0530

    Relational Operators

---
+++ RelationalOperators.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+public class RelationalOperators {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a = input.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = input.nextInt();
+
+        System.out.println("a == b: " + (a == b));
+        System.out.println("a != b: " + (a != b));
+        System.out.println("a > b: " + (a > b));
+        System.out.println("a < b: " + (a < b));
+        System.out.println("a >= b: " + (a >= b));
+        System.out.println("a <= b: " + (a <= b));
+
+    }
+}
