37e374326b6b0334c5034a90a9818678f8864e61
Saya1206 <sayali.patil0831>
2025-05-26 10:50:56 +0530

    Java Comments

---
+++ Comments.java
@@ -0,0 +1,16 @@
+
+public class Comments {
+
+    public static void main(String[] args) {
+
+        // single-line comment
+
+        /*
+    multi-line comment
+    -----------------------
+    -----------------------
+    -----------------------
+     
+         */
+    }
+}
