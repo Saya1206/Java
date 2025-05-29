ad4b22a040342bed3e967d35fac8d434e907e5cd
Saya1206 <sayali.patil0831>
2025-05-29 14:20:54 +0530

    Challenge 29

---
+++ HalfPyramidPattern.java
@@ -0,0 +1,16 @@
+
+public class HalfPyramidPattern {
+
+    public static void main(String[] args) {
+        int rows = 5;
+
+        // outer loop
+        for (int i = 1; i <= rows; i++) {
+            // inner loop
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
