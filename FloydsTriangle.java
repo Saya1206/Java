097f9db0fd4faefebdc1845738ce93ead7648b30
Saya1206 <sayali.patil0831>
2025-05-30 13:04:14 +0530

    Challenge 34

---
+++ FloydsTriangle.java
@@ -0,0 +1,18 @@
+
+public class FloydsTriangle {
+
+    public static void main(String[] args) {
+        int rows = 5;
+        int number = 1;
+
+        // outer loop
+        for (int i = 1; i <= rows; i++) {
+            // inner loop
+            for (int j = 1; j <= i; j++) {
+                System.out.print(number + " ");
+                number++;
+            }
+            System.out.println();
+        }
+    }
+}
