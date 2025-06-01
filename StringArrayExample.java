dac7b53026d21d5f7797db8e655825e3a9cc9ee8
Saya1206 <sayali.patil0831>
2025-06-01 21:26:24 +0530

    Challenge 38

---
+++ StringArrayExample.java
@@ -0,0 +1,14 @@
+
+public class StringArrayExample {
+
+    public static void main(String[] args) {
+        String[] names = {"Sneha", "Rohan", "Vedika", "Vikrant", "Raj", "Tejal"};
+
+        System.out.println("List of names: ");
+
+        //Print all names using a for-each loop
+        for (String name : names) {
+            System.out.println(name);
+        }
+    }
+}
