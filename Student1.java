a92bebdda0c10bea26e313109bfa7b633e4d72c2
Saya1206 <sayali.patil0831>
2025-07-16 23:45:40 +0530

    this keyword

---
+++ Student1.java
@@ -0,0 +1,12 @@
+public class Student1 {
+  int x;
+
+  Student1(int x) {
+    this.x = x;
+  }
+
+  public static void main(String[] args) {
+    Student1 obj = new Student1(5);
+    System.out.println("Value of x is: " + obj.x);
+  }
+}
