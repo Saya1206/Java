d424ab0385a10d87715526e307a7d49fb29e9984
Saya1206 <sayali.patil0831>
2025-07-16 12:22:14 +0530

    continue statement

---
+++ DemoContinue.java
@@ -0,0 +1,14 @@
+
+public class DemoContinue {
+
+    public static void main(String[] args) {
+        System.out.println("Start");
+        for (int i = 1; i <= 10; i++) {
+            if (i == 5) {
+                continue;
+            }
+            System.out.println(i);
+        }
+        System.out.println("End");
+    }
+}
